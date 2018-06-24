e6d45aac9d19adb74b540f551b753be80d83f683
unknown <monmaramba@yahoo.com>
2018-06-24 08:56:24 -0700

    on click stuff plus search vars on the way

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,56 @@
 //key uHY92bdnYhXNwpf69CggQTfGW79oMQ1N
+//initial array of sports champions
+var sportsChamps=["Michael Schumacher", "Tiger Woods", "Roger Federer", "Chris Froome",  "Venus Williams", "Manny Pacquiao", "Lindsey Vonn"];
+
+//creates divs to hold buttons and gifs
+    //var buttonHolder = $('<div>');
+    var gifHolder = $('<div>');
+    var i;
+    var athletes;
+    var button;
+
+for (i=0; i < sportsChamps.length; i++){
+    /*$('<button/>'), {
+        text: sportsChamps[i];
+        id: 'btn' + i,
+        click; function() {
+            alert(this.text);
+        }
+    }*/
+    //$("buttonHolder").empty();
+    $("")
+    athletes = sportsChamps[i];
+    button = $('<button>' + sportsChamps[i] + '</button>');
+    button.attr("id", sportsChamps[i]);
+    //button.attr("name", sportsChamps[i]);
+    button.text(athletes);
+    $(".container").append(button);
+    
+}; 
+
+//function for onclick event to request for giphy
+    
+    var firstName;
+    var lastName;
+
+$("button").on('click', function(){
+    event.preventDefault();
+    //$("button").attr(this.id);
+    var champString = (this.id);
+    var champArr = champString.split(" ");
+    firstName = champArr[0];
+    lastName = champArr[1];
+
+    console.log(firstName);
+    console.log(lastName);
+});
+
+//$(HTMLBodyElement).append(buttonHolder);
+
+//create div to render buttons into
+
+//Function to render champion buttons
+
+
+
+//Function to  
