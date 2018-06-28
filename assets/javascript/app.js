b4c35f73e4f7540c44c5510da2db091b1f11934a
unknown <monmaramba@yahoo.com>
2018-06-27 19:13:27 -0700

    pause-play working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,41 @@
 //key uHY92bdnYhXNwpf69CggQTfGW79oMQ1N
 //initial array of sports champions
-var sportsChamps=["Michael Schumacher", "Tiger Woods", "Roger Federer", "Chris Froome",  "Venus Williams", "Manny Pacquiao", "Lebron James"];
+var sportsChamps=["Michael Schumacher", "Tiger Woods", "Roger Federer", "Chris Froome",  "Venus Williams", "Manny Pacquiao",];
 
-//creates divs to hold buttons and gifs
-    //var buttonHolder = $('<div>');
+//create divs to hold buttons and gifs plus variable declaration
+    var buttonHolder = $('<div>');
     var gifHolder = $('<div>');
-    var i;
+    var star="";
     var athletes;
     var button;
 
-for (i=0; i < sportsChamps.length; i++){
+function renderButton() {
+    $(buttonHolder).empty();
+
+    for (i=0; i < sportsChamps.length; i++){
    
-    $("buttonHolder").empty();
-    
     athletes = sportsChamps[i];
     button = $('<button>' + sportsChamps[i] + '</button>');
     button.attr("id", sportsChamps[i]);
     //button.attr("name", sportsChamps[i]);
     button.text(athletes);
-    $(".container").append(button);
+    $(buttonHolder).append(button);
+    $(".container").append(buttonHolder);
     
-}; 
+} 
+};
+renderButton();
 
-//function for onclick event to request for giphy
+
+//function for onclick event to request from giphy
     
     var firstName;
     var lastName;
-
+    var champArr = [];
 $("button").on('click', function(){
     event.preventDefault();
     var champString = (this.id);
-    var champArr = champString.split(" ");
+    champArr = champString.split(" ");
     firstName = champArr[0];
     lastName = champArr[1];
 
@@ -51,61 +56,50 @@ $("button").on('click', function(){
 
             var results = response.data;
 
-            for (var i=0; i < results.length; i++){
+            for (var j=0; j < results.length; j++){
 
-                var gifURL = $("<img>");
-                gifURL.attr("src", results[i].images.downsized_still.url);
-                gifHolder.prepend(gifURL);
+                var gifIMG = $("<img>");
+                //gifURL.attr("src", results[j].images.fixed_height_still.url);
+                gifIMG.attr({
+                    "src": results[j].images.fixed_height_still.url,
+                    "data-state": "still",
+                    "data-still": results[j].images.fixed_height_still.url,
+                    "data-animate": results[j].images.fixed_height.url,
+                    });
 
-                var rating = results[i].rating;
+                gifIMG.on("click", function() {
+
+                    var state = $(this).attr("data-state");
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    }
+                })  
+
+                gifHolder.prepend(gifIMG);
+
+                var rating = results[j].rating;
                 var p1 = $("<p>").text("Rating: " + rating);
                 gifHolder.prepend(p1);
                 console.log(rating);
-                
-
-
-               /* var rating = response[i].rating;
-                var pR = $("<p>").text("Rating: " + rating);
-                gifHolder.append(pR);
-                console.log(response[i].rating);
-
-
-                var gifURL = response.images.downsized_still;
-                gifURL.attr("data-state", "still");
-                gifURL.attr("class", "gif");
-                var annimGifURL = response.images.downsized;
-                annimGifURL.attr("data-state", "animate");
-                annimGifURL.attr("class", "gif");
-                
-                var gifDisp = $("<img>");
-                gifDisp.attr("src", gifURL);*/
-
-                /*$(".gif").on("click", function() {
-                    var state = $(this).attr("data-state");
-
-                    if (state === "still") {
-                        $(this).attr("src", $(this).attr("data-state"));
-                    }
-                })*/
-
-
-                
-
-            } 
-            
-
-
-        })
-    
+                        } 
+                 })
 });
+
+$("#addChamp").on("click", function() {
+    event.preventDefault();
+    // Input from html form
+    star = $("#sportsStar").val().trim;
+
+    sportsChamps.push(star);
+
+    renderButton();
+  });
+
+//renderButton();
 gifHolder.insertAfter(".container");
 
-//$(HTMLBodyElement).append(buttonHolder);
 
-//create div to render buttons into
-
-//Function to render champion buttons
-
-
-
-//Function to  
